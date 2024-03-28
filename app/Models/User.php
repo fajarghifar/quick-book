477f700c5915f2f7eb2c463e29fcb50c1d678eda
Fajar Ghifari <fajarghifari3@gmail.com>
2024-03-28 21:05:17 +0700

    FEAT: Add initial database schema for the project

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
+        'company_id',
     ];
 
     /**
@@ -42,4 +44,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
